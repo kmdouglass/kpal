1c65677b0015a8f84118ffb70b7d28e3263fde67
kmdouglass <kyle.m.douglass@gmail.com>
2019-05-04 10:37:07 +0200

    Added the library loader routine

--- src/utils.rs
+++ src/utils.rs
@@ -3,6 +3,9 @@ use std::fs::read_dir;
 use std::io::Error;
 use std::path::{Path, PathBuf};
 
+use libloading::Library;
+use log;
+
 /// Finds all plugin library files inside a directory.
 ///
 /// # Arguments
@@ -30,5 +33,35 @@ pub fn find_plugins(dir: &Path) -> Result<Option<Vec<PathBuf>>, Error> {
         Ok(Some(plugins))
     } else {
         Ok(None)
+    }
+}
+
+/// Loads a list of plugin library files.
+///
+/// # Arguments
+///
+/// * `libs` - A vector of `PathBuf`s pointing to library files to load.
+pub fn load_plugins(libs: Vec<PathBuf>) -> Option<Vec<Library>> {
+    let mut plugins: Vec<Library> = Vec::new();
+    for lib in libs {
+        log::info!("Attempting to load library from file: {:?}", lib);
+        let lib = match Library::new(&lib) {
+            Ok(lib) => {
+                log::info!("Succeeded to load library from file: {:?}", lib);
+                lib
+            }
+            Err(_) => {
+                log::error!("Failed to load library from file: {:?}", lib);
+                continue;
+            }
+        };
+
+        plugins.push(lib);
+    }
+
+    if plugins.len() != 0 {
+        Some(plugins)
+    } else {
+        None
     }
 }
--- tests/utils_integration.rs
+++ tests/utils_integration.rs
@@ -2,11 +2,16 @@ use std::fs::File;
 use std::io::Error;
 use std::path::PathBuf;
 
+use env_logger;
 use tempfile::{tempdir, TempDir};
 
-use kpal::utils::find_plugins;
+use kpal::utils::{find_plugins, load_plugins};
 
-fn create_files(dir: &TempDir, files: Vec<&str>) -> Result<Vec<PathBuf>, Error> {
+fn set_up() {
+    let _ = env_logger::builder().is_test(true).try_init();
+}
+
+fn create_dummy_files(dir: &TempDir, files: Vec<&str>) -> Result<Vec<PathBuf>, Error> {
     let path = dir.path();
     let mut libs: Vec<PathBuf> = Vec::new();
     for file in files.iter() {
@@ -21,8 +26,10 @@ fn create_files(dir: &TempDir, files: Vec<&str>) -> Result<Vec<PathBuf>, Error> 
 /// find_plugins works when only library files are present.
 #[test]
 fn find_plugins_library_files_only() {
+    set_up();
+
     let dir = tempdir().expect("Could not create temporary directory for test data.");
-    let libs: Vec<PathBuf> = create_files(&dir, vec!["plugin_1.so", "plugin_2.so"])
+    let libs: Vec<PathBuf> = create_dummy_files(&dir, vec!["plugin_1.so", "plugin_2.so"])
         .expect("Could not create test data files");
 
     let result = find_plugins(dir.path()).expect("Call to find_plugins resulted in an error.");
@@ -40,9 +47,12 @@ fn find_plugins_library_files_only() {
 /// find_plugins works when library files and other file types are present.
 #[test]
 fn find_plugins_mixed_file_types() {
+    set_up();
+
     let dir = tempdir().expect("Could not create temporary directory for test data.");
-    let libs: Vec<PathBuf> = create_files(&dir, vec!["plugin_1.so", "plugin_2.so", "data.txt"])
-        .expect("Could not create test data files");
+    let libs: Vec<PathBuf> =
+        create_dummy_files(&dir, vec!["plugin_1.so", "plugin_2.so", "data.txt"])
+            .expect("Could not create test data files");
 
     let result = find_plugins(dir.path()).expect("Call to find_plugins resulted in an error.");
     let mut found_libs = match result {
@@ -59,9 +69,47 @@ fn find_plugins_mixed_file_types() {
 /// find_plugins returns None when no library files are present.
 #[test]
 fn find_plugins_no_plugin_library_files() {
+    set_up();
+
     let dir = tempdir().expect("Could not create temporary directory for test data.");
-    create_files(&dir, vec!["data.txt"]).expect("Could not create test data files");
+    create_dummy_files(&dir, vec!["data.txt"]).expect("Could not create test data files");
 
     let result = find_plugins(dir.path()).expect("Call to find_plugins resulted in an error.");
     assert_eq!(None, result);
+}
+
+/// load_plugins works for a list of correct library files.
+#[test]
+fn load_plugins_loads_library_files() {
+    set_up();
+
+    let mut lib = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    lib.push("target/debug/examples/libbasic-peripheral.so");
+
+    let mut libs: Vec<PathBuf> = Vec::new();
+    libs.push(lib);
+
+    let result = load_plugins(libs);
+    match result {
+        Some(_) => (),
+        None => panic!("load_plugins failed to load any libraries."),
+    }
+}
+
+/// load_plugins does not return library files that do not exist.
+#[test]
+fn load_plugins_handles_missing_library_files() {
+    set_up();
+
+    let mut lib = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    lib.push("target/debug/examples/fake_library.so");
+
+    let mut libs: Vec<PathBuf> = Vec::new();
+    libs.push(lib);
+
+    let result = load_plugins(libs);
+    match result {
+        Some(_) => panic!("load_plugins found libraries that it should not have."),
+        None => (),
+    }
 }
